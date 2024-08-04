ba7baa599b350703ac91f59fe4a4af91b1ea8895
hannahschwen <hannahschwen@gmail.com>
2024-08-04 13:52:46 -0400

    wrote post route for new post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
@@ -15,5 +16,17 @@ router.get('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create( {
+      ...req.body,
+      user_id: req.session.user_id,
+    })
+  } catch(err) {
+    res.status(400).json(err);
+  }
+})
+
 
 module.exports = router;
